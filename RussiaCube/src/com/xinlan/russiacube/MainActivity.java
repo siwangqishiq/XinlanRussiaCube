e90b6c7ae86f63037f09ec0b01a347bb21470fc2
siwangqishiq <525647740@qq.com>
2013-02-17 14:03:56 +0800

    框架搭建

---
+++ RussiaCube/src/com/xinlan/russiacube/MainActivity.java
@@ -0,0 +1,20 @@
+package com.xinlan.russiacube;
+
+import com.xinlan.russiacube.view.MainView;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new MainView(this));
+    }
+}
---
+++ RussiaCube/src/com/xinlan/russiacube/view/MainView.java
@@ -0,0 +1,116 @@
+package com.xinlan.russiacube.view;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+
+public class MainView extends SurfaceView implements Callback, Runnable {
+	private SurfaceHolder sfh;
+	private Paint paint;
+	private Thread th;
+	private boolean flag;
+	private Canvas canvas;
+	private Context context;
+	public static int screenW, screenH;
+	private Resources res = this.getResources();
+	
+	public static int GAME_STATE=1;
+
+	public MainView(Context context) {
+		super(context);
+		this.context = context;
+		sfh = this.getHolder();
+		sfh.addCallback(this);
+		paint = new Paint();
+		paint.setColor(Color.WHITE);
+		paint.setAntiAlias(true);
+		setFocusable(true);
+		setFocusableInTouchMode(true);
+		// 设置背景常亮
+		this.setKeepScreenOn(true);
+	}
+
+	public void surfaceCreated(SurfaceHolder holder) {
+		screenW = this.getWidth();
+		screenH = this.getHeight();
+		init();
+		flag = true;
+		// 实例线程
+		th = new Thread(this);
+		// 启动线程
+		th.start();
+	}
+
+	/**
+	 * 初始化
+	 */
+	public void init() {
+		GAME_STATE=1;
+	}
+	
+	public void main(){
+		try {
+			canvas = sfh.lockCanvas();
+			if (canvas != null) {
+				switch(GAME_STATE){
+				case 1:
+					canvas.drawColor(Color.BLACK);
+					draw();
+					logic();
+					break;
+				}
+			}// end if
+		} catch (Exception e) {
+		} finally {
+			if (canvas != null) {
+				sfh.unlockCanvasAndPost(canvas);
+			}
+		}
+	}
+
+	public void draw() {
+		
+	}
+
+	public void logic() {
+	}
+
+	public void run() {
+		while (flag) {
+			long start = System.currentTimeMillis();
+			main();
+			long end = System.currentTimeMillis();
+			//System.out.println(end - start);
+			try {
+				if (end - start < 30) {
+					Thread.sleep(30- (end - start));
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}// end while
+	}
+
+	/**
+	 * 监听触屏事件
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		//mController.onTouch(event);
+		return true;
+	}
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		flag = false;
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+}// end class
